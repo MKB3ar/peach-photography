fb74d457609d7ea06ca6e64675cd518cd9596205
TeamBearCrew <pandakenfire@gmail.com>
2025-04-01 13:21:44 +0300

    Change default port and host

--- web_project/settings.py
+++ web_project/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        'HOST': os.getenv("POSTGRES_HOST"),
-        'PORT': os.getenv("POSTGRES_POST")
+        'HOST': os.getenv("POSTGRES_HOST", 'localhost'),
+        'PORT': os.getenv("POSTGRES_PORT", '5433')
     }
 }
 
