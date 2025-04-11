735a63588c73b451c41a6b71a3ba9d1b3f05aaf2
TeamBearCrew <pandakenfire@gmail.com>
2025-04-11 11:53:56 +0300

    Create simple view for photo upload

--- project_cv_yolo/views.py
+++ project_cv_yolo/views.py
@@ -1,5 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
 
+from .forms import PhotoModelForm
 # Create your views here.
+
+def upload_photo(request):
+    if request.method == 'POST':
+        form = PhotoModelForm(
+            request.POST,
+            request.FILES
+        )
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.save()
+            return HttpResponse('Всё успешно загружено!')
+    else:
+        form = PhotoModelForm()
+    return render(
+        request=request,
+        context={'form':form},
+        template_name='project_cv_yolo/upload.html')
 
