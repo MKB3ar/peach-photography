d1c057a5c8656d4f735536980709b2d64dcab08d
TeamBearCrew <pandakenfire@gmail.com>
2025-04-15 14:02:32 +0300

    Add opencv to urls

--- project_cv_yolo/urls.py
+++ project_cv_yolo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('upload/', views.upload_photo, name='upload_photo')
+    path('upload/', views.upload_photo, name='upload_photo'),
+    path('opencv/', views.photo_mask, name='photo_mask')
 ]
-
