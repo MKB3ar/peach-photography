425c2419b0793250efcb15fb9760ee5be603d37f
TeamBearCrew <pandakenfire@gmail.com>
2025-04-29 17:04:45 +0300

    Add different routes to project

--- project_cv_yolo/urls.py
+++ project_cv_yolo/urls.py
@@ -5,5 +5,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('upload/', views.upload_photo, name='upload_photo'),
-    path('opencv/', views.photo_mask, name='photo_mask')
+    path('opencv/', views.photo_mask, name='photo_mask'),
+    path('video/', views.video, name='video'),
+    path('detections/', views.detections_api, name='detections_api'),
+    path('video_feed/', views.video_feed, name='video_feed'),
+    path('', views.welcome, name='welcome')
 ]
