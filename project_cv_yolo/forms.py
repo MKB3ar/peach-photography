cd20aa92b2132a36efb543a5c649ddced1c0042f
TeamBearCrew <pandakenfire@gmail.com>
2025-04-30 11:21:56 +0300

    Add video form to forms.py

--- project_cv_yolo/forms.py
+++ project_cv_yolo/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
-from .models import PhotoModel
+from .models import PhotoModel, VideoModel
 
 #Form for photo upload
 class PhotoModelForm(forms.ModelForm):
     class Meta:
         model = PhotoModel
         fields = ['title', 'image']
+        
+class VideoForm(forms.ModelForm):
+    class Meta:
+        model = VideoModel
+        fields = ['title', 'video']
