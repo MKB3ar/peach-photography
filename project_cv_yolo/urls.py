ddfc41a9249ebc6c281eeef9b367a194650d1161
TeamBearCrew <pandakenfire@gmail.com>
2025-04-30 11:22:44 +0300

    Add different paths to urls

--- project_cv_yolo/urls.py
+++ project_cv_yolo/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('upload/', views.upload_photo, name='upload_photo'),
     path('opencv/', views.photo_mask, name='photo_mask'),
     path('video/', views.video, name='video'),
+    path('upload-video/', views.upload_video, name='upload_video'),
+    path('video-feed/', views.video_feed, name='video_feed'),
     path('detections/', views.detections_api, name='detections_api'),
-    path('video_feed/', views.video_feed, name='video_feed'),
-    path('', views.welcome, name='welcome')
+    path('', views.welcome, name='welcome'),
+    path('gallery/', views.gallery_view, name='gallery'),
+    path('gallery/delete/<int:photo_id>/', views.delete_photo, name='delete_photo'),
 ]
