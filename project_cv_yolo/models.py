4767ccb8401340885b477dfeded6d9b0e6287099
TeamBearCrew <pandakenfire@gmail.com>
2025-04-30 11:22:24 +0300

    Add some delete logic and video model to models

--- project_cv_yolo/models.py
+++ project_cv_yolo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import FileExtensionValidator
+from django.core.files.storage import default_storage
 
 # Create your models here.
 
@@ -16,6 +18,18 @@ class PhotoModel(models.Model):
         default=timezone.now
     )
     
+    def delete(self, *args, **kwargs):
+    # Удаляем основное фото
+        if self.image:
+            if default_storage.exists(self.image.name):
+                default_storage.delete(self.image.name)
+        # Удаляем все связанные ProcessedPhoto и их файлы
+        for processed in self.processedphoto_set.all():
+            if default_storage.exists(processed.processed_photo.name):
+                default_storage.delete(processed.processed_photo.name)
+            processed.delete()
+        super().delete(*args, **kwargs)
+    
     def __str__(self):
         return self.title + ' ' + str(self.image)
     
@@ -26,7 +40,7 @@ class MaskType(models.Model):
 #Model for processed photo with cv2
 class ProcessedPhoto(models.Model):
     original_photo = models.ForeignKey(
-        PhotoModel, on_delete=models.RESTRICT
+        PhotoModel, on_delete=models.CASCADE
     )
     processed_photo = models.ImageField(
         upload_to='processed_photo/',
@@ -38,3 +52,9 @@ class ProcessedPhoto(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True
     )
+
+class VideoModel(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    video = models.FileField(upload_to='video/',
+                             validators=[FileExtensionValidator(['mp4'])])
+    uploaded_at = models.DateTimeField(default=timezone.now)
